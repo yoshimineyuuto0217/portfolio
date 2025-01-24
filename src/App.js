71ebe382996f965db10c461048240454e6bb17fd
吉嶺勇斗 <jilingyongdou@gmail.com>
2025-01-25 00:41:53 +0900

    追加

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@ import About from "./pages/About";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact";
 import "./styles/res.min.css";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
+import template from "./particlesjs-config .json";
 
 const App = () => {
+  const params = template;
+  const particlesInit = useCallback(async (engine) => {
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
   return (
     <Router>
       <main>
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={params}
+    />
         <div className="container">
           <div className="side">
             <SideBar />
@@ -20,7 +38,7 @@ const App = () => {
               <Route path="/" element={<MainView />} />
               <Route path="/works" element={<Work />} />
               <Route path="/about" element={<About />} />
-              <Route path="/contact" element={<Contact />} />
+              <Route path="/contact" element={<Contact/>} />
             </Routes>
             <div className="footer">
               <small>&copy;YOSHIMINE YUUTO</small>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,66 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from 'react';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+  const [message, setMessage] = useState('');
+  const [emailSent, setEmailSent] = useState(false);
+  const [isSending, setIsSending] = useState(false);
+
+  const handleName =(event)=> {
+    setName(event.target.value);
+  };
+  const handleEmail =(event)=> {
+    setEmail(event.target.value);
+  };
+  const handleNumber =(event)=> {
+    setNumber(event.target.value);
+  };
+  const handleMessage =(event)=> {
+    setMessage(event.target.value);
+  };
+  const handleSubmit = async(event) => {
+    event.preventDefault();
+    try {
+      setIsSending(true);
+      // 送信する処理
+      await axios.post('http://localhost:3001/send-mail', {
+          email:email.trim(),
+          message: message,
+          number: number,
+      })
+      console.log("メールが送信されました。")
+      // メール送信が終わったあと
+      setEmailSent(true);
+  } catch (error) {
+      console.log(error);
+  } finally {
+      setIsSending(false);
+  };
+  console.log('送信されたメール：', email)
+  console.log('送信されたメッセージ：', message)
+  };
+
   return (
     <>
-    <h2>Contact</h2>
-    <form className='common contact'> 
+      <h2>Contact</h2>
+    <form className='common contact'  onSubmit={(event) =>handleSubmit(event)}> 
     <label for="name">Name</label>
-    <input type='text'></input>
+    <input type='text' onChange={handleName} value={name}></input>
     <label for="email">E-mail</label>
-    <input type='text'></input>
-    <label for="number">Phone-number</label>
-    <input type='text'></input>
+    <input type='text' onChange={handleEmail} value={email}></input>
+    <label for="number" >Phone-number</label>
+    <input type='text' value={number} onChange={handleNumber}></input>
     <label for="number">Message</label>
-    <textarea id="message" name="message" rows="20" cols="100"></textarea>
+    <textarea value={message} onChange={handleMessage}id="message" name="message" rows="20" cols="100"></textarea>
     <div className='submit common'>
-    <button>submit</button>
+    <button type="submit">submit</button>
     </div>
-    </form>
+    </form> 
     </>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
